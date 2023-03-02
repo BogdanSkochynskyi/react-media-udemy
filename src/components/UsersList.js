876dfab6e8bc50965cc9188c89d407a7189fa8b4
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-03-02 14:27:53 +0200

    Added a loading stage

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,30 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchUsers} from "../store";
+import Skeleton from "./Skeleton";
+
 function UsersList() {
+    const dispatch = useDispatch();
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users;
+    })
+
+    useEffect(()=> {
+        dispatch(fetchUsers());
+    },[dispatch])
+
+    if (isLoading) {
+        return <div>
+            <Skeleton times={6} className="h-10 w-full"/>
+        </div>
+    }
+
+    if(error) {
+        return <div>Error fetching data...</div>
+    }
+
     return (
-        <div>Users List</div>
+        <div>{data.length}</div>
     )
 }
 
