189ad01def1d4e968ffa4db3c22cb1afc76361ca
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-03-06 13:45:56 +0200

    Added an api to fetch albums and to add album via redux toolkit query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,42 @@
+import {useAddAlbumMutation, useFetchAlbumsQuery} from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
+
+    const{data, error, isLoading} = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    }
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3}/>
+    } else if (error) {
+        content = <div>Error</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos
+            </ExpandablePanel>
+        });
+    }
+
     return <div>
-        Albums for {user.name}
+        <div>
+            Albums for {user.name}
+            <Button onClick={handleAddAlbum}>
+                + add album
+            </Button>
+        </div>
+        <div>
+            {content}
+        </div>
+
     </div>
 }
 
